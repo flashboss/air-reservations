a0d817031a1f68148f63b85533eec81926d5b9b4
flashboss <luca.stancapiano@vige.it>
2016-08-07 11:19:46 +0200

    license

--- src/main/java/org/activiti/explorer/ui/task/TaskMenuBar.java
+++ src/main/java/org/activiti/explorer/ui/task/TaskMenuBar.java
@@ -1,3 +1,16 @@
+/******************************************************************************
+ * Vige, Home of Professional Open Source Copyright 2010, Vige, and           *
+ * individual contributors by the @authors tag. See the copyright.txt in the  *
+ * distribution for a full listing of individual contributors.                *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
+ * not use this file except in compliance with the License. You may obtain    *
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0        *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
 package org.activiti.explorer.ui.task;
 
 import static it.vige.reservations.DemoData.isAdmin;
@@ -36,8 +49,7 @@ import com.vaadin.ui.Button.ClickListener;
 /**
  * The menu bar which is shown when 'Tasks' is selected in the main menu.
  * 
- * @author Joram Barrez
- * @author Frederik Heremans
+ * @author lucastancapiano
  */
 public class TaskMenuBar extends ToolBar {
 
